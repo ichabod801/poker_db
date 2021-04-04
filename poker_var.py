a38c8aa38e5a67fc4ad3a50033a6b6252e8599c1
Craig O'Brien <cobrien@xenomind.com>
2021-04-04 15:19:32 -0400

    Got the base cmd instance written, with the usual mods.

--- poker_var.py
+++ poker_var.py
@@ -3,6 +3,9 @@ poker_db.py
 
 A command line interface for Ichabod's Poker Variant Database.
 
+WARNING: This program uses eval in Viewer.do_shell. Before running this program
+over a network or other insecure situations, you should nerf that method.
+
 To Do:
 * Restart git, with a remote on github.
 * Convert to sqlite.
@@ -34,4 +37,103 @@ To Do:
 		* I'm really leaning to a second variable.
 	* Look at distance analysis again.
 		* Currently I like the idea of a tree starting with Showdown Straight.
+
+Classes:
+Viewer: A command line interface for Ichabod's Poker Variant Database. (cmd.Cmd)
 """
+
+import cmd
+import traceback
+
+class Viewer(cmd.Cmd):
+	"""
+	A command line interface for Ichabod's Poker Variant Database. (cmd.Cmd)
+
+	Class Attributes:
+	aliases: Command aliases. (dict of str: str)
+
+	Methods:
+	do_quit: Quit the interface. (True)
+
+	Overridden Methods:
+	default
+	do_shell
+	onecmd
+	precmd
+	preloop
+	postcmd
+	"""
+
+	aliases = {'q': 'quit'}
+	prompt = 'IPVDB >> '
+
+	def default(self, line):
+		"""
+		Handle unrecognized input. (bool)
+
+		Parameters:
+		line: The command entered by the user. (str)
+		"""
+		words = line.split()
+		if words[0] in self.aliases:
+			words[0] = self.aliases[words[0]]
+			return self.onecmd(' '.join(words))
+		else:
+			return super(Viewer, self).default(line)
+
+	def do_quit(self, arguments):
+		"""Quit the IPVDB interface. (q)"""
+		return True
+
+	def do_shell(self, arguments):
+		"""Handle raw Python code. (!)"""
+		print(eval(arguments))
+
+	def onecmd(self, line):
+		"""
+		Interpret the argument. (str)
+
+		Parameters:
+		line: The line with the user's command. (str)
+		"""
+		# Catch errors and print the traceback.
+		try:
+			return super(Viewer, self).onecmd(line)
+		except:
+			traceback.print_exc()
+
+	def precmd(self, line):
+		"""
+		Pre-command processing. (str)
+
+		Parameters:
+		line: The line with the user's command. (str)
+		"""
+		print()
+		return line
+
+	def preloop(self):
+		"""Set up the interface. (None)"""
+		self.load_data()
+		self.current = self.variants['Five Card Draw']
+		self.current_rule = self.rules[950]   # One-eyed jacks are wild.
+		self.changed = set()
+		self.count, self.match_index = 0, 0
+		self.matches, self.rule_matches = [], []
+		print()
+
+	def postcmd(self, stop, line):
+		"""
+		Post-command processing. (str)
+
+		Parameters:
+		stop: A flag for quitting the interface. (bool)
+		line: The line with the user's command. (str)
+		"""
+		if not stop:
+			print()
+		return stop
+
+if __name__ == '__main__':
+	viewer = Viewer()
+	viewer.cmdloop()
